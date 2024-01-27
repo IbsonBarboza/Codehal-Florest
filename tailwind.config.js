5e8f24c029a4b25e4e9e60fd7f3000c1f3724266
Ibson Barboza de Oliveira <128850394+IbsonBarboza@users.noreply.github.com>
2024-01-26 20:02:14 -0500

    forest

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        backgrundT: 'rgb(255, 255, 255, .1)',
+      },
+    },
   },
   plugins: [],
 }
